7c571e2aa8443cfb4436b9a92ced165ea35ec0fb
pommedapi7749 <mayeul.boeri@gmail.com>
2023-04-13 16:45:48 +0200

    patched update request

--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authController = require('../controllers/products')
 
 router.post('/', authController.registerProduct);
-router.post('/:productId', authController.updateProduct);
+router.put('/:productId', authController.updateProduct);
 router.get('/', authController.getAllProducts);
 router.get('/:productId', authController.getProduct);
 router.delete('/:productId', authController.deleteProduct);
