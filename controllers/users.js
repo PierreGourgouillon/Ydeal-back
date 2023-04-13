5e5a8b2515b52d8b43dcefdc9ca27e2c7f4a8929
sam <sam.bernolle@ynov.com>
2023-04-13 16:35:42 +0200

    getuser finish

--- controllers/users.js
+++ controllers/users.js
@@ -19,6 +19,40 @@ exports.registerUser = async (req, res, next) => {
     }
 }
 
+exports.getUsers = async (req, res, next) =>{
+    try {
+        const users = await User.find()
+        res.status(200).json({
+            error: null,
+            data: users
+        });
+    
+    }catch(error){
+        res.status(400).json({
+            error:  "INTERNAL_ERROR" ,
+            data: null
+        }) 
+    } 
+}
+
+exports.getUserId = async (req, res, next)=>{
+    try{
+        const userId = req.params.userId
+        const userfound = await User.find({firebaseId: userId})
+        //console.log()
+        res.status(200).json({
+            error: null,
+            data: userfound
+        });
+    }catch(error){
+        res.status(400).json({
+            error:  "INTERNAL_ERROR" ,
+            data: null
+        }) 
+    } 
+}
+
+
 exports.updateUser = async (req, res, next) => {
     const filter = { firebaseId: req.params.userId };
     const update = { ...req.body };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/users')
+const authController = require('../controllers/users');
+const { auth } = require('firebase-admin');
 
 router.post('/', authController.registerUser);
 router.post('/:userId', authController.updateUser);
 // router.get('/me', authController.me);
+router.get('/', authController.getUsers);
+router.get('/:userId', authController.getUserId);
 
 module.exports = router;
