dfb8d302f37ed7ffe886ad3e08ede6b93586bf81
pommedapi7749 <mayeul.boeri@gmail.com>
2023-04-13 14:25:28 +0200

    Added DDN

--- models/userModel.js
+++ models/userModel.js
@@ -9,6 +9,7 @@ const userSchema = mongoose.Schema({
     pseudo: { type: String, require: true },
     description: {type: String, require: true},
     campus: { type: String, require: true },
+    birthDay: { type: String, require: true },
 })
 
 module.exports = mongoose.model("User", userSchema);
