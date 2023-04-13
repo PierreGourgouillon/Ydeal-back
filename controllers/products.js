bbd4890222eda8fe9ce73e7e5077ba0e19a4537d
PommeD'API <63107554+PommeDAPI7749@users.noreply.github.com>
2023-04-13 17:25:14 +0200

    Feature/products (#5)
    * create, get, update, delete
    
    * patched update request
    
    * removed comment
    
    * Added owner check
    
    * Return product at creation

---
+++ controllers/products.js
@@ -0,0 +1,95 @@
+const Product = require('../models/productModel');
+
+exports.registerProduct = async (req, res, next) => {
+    let product = new Product({
+        ...req.body,
+        ownerId: req.user.firebaseId
+    })
+    try {
+        await product.save().then(savedProduct => { product = savedProduct})
+        res.status(201).json({
+            error: null,
+            data: product
+        });
+    } catch (error) {
+        res.status(400).json({
+            error: "INTERNAL_ERROR",
+            data: {}
+        });
+    }
+}
+
+exports.getProduct = async (req, res, next) => {
+    const filter = { _id: req.params.productId };
+
+    try {
+        let product = await Product.findOne(filter);
+        res.status(201).json({
+            error: null,
+            data: product
+        });
+    } catch (error) {
+        res.status(400).json({
+            error: "INTERNAL_ERROR",
+            data: {}
+        });
+    }
+}
+
+exports.getAllProducts = async (req, res, next) => {
+    try {
+        let products = await Product.find({});
+        res.status(201).json({
+            error: null,
+            data: products
+        });
+    } catch (error) {
+        res.status(400).json({
+            error: "INTERNAL_ERROR",
+            data: {}
+        });
+    }
+}
+
+exports.updateProduct = async (req, res, next) => {
+    const filter = { _id: req.params.productId };
+    const update = { ...req.body };
+
+    try {
+        let productUpdated = await Product.findOneAndUpdate(filter, update, {new: true});
+        res.status(201).json({
+            error: null,
+            data: productUpdated
+        });
+    } catch (error) {
+        res.status(400).json({
+            error: "INTERNAL_ERROR",
+            data: {}
+        });
+    }
+}
+
+exports.deleteProduct = async (req, res, next) => {
+    const filter = { _id: req.params.productId };
+
+    try {
+        const product = Product.findOne(filter);
+        if (product.ownerId != req.user.firebaseId) {
+            return res.status(401).json({
+                error: "NOT_OWNER_ERROR",
+                data: {}
+            });
+        }
+        await Product.deleteOne(filter);
+        res.status(201).json({
+            error: null,
+            data: {}
+        });
+    } catch (error) {
+        res.status(400).json({
+            error: "INTERNAL_ERROR",
+            data: {}
+        });
+    }
+}
+
---
+++ routes/products.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/products')
+
+router.post('/', authController.registerProduct);
+router.put('/:productId', authController.updateProduct);
+router.get('/', authController.getAllProducts);
+router.get('/:productId', authController.getProduct);
+router.delete('/:productId', authController.deleteProduct);
+
+module.exports = router;
