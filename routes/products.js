3b2e800962011b1c69c00aa417c81433e2acbe69
Pierre <Pierre_gourgouillon@hotmail.com>
2023-04-14 10:42:44 +0200

    Modify ProcFile

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/products')
+const authController = require('../controllers/products');
+const authMiddleware = require('../middlewares/auth');
 
-router.post('/', authController.registerProduct);
-router.put('/:productId', authController.updateProduct);
+router.post('/', authMiddleware, authController.registerProduct);
+router.put('/:productId', authMiddleware, authController.updateProduct);
 router.get('/', authController.getAllProducts);
 router.get('/:productId', authController.getProduct);
-router.delete('/:productId', authController.deleteProduct);
+router.delete('/:productId', authMiddleware, authController.deleteProduct);
 
 module.exports = router;
